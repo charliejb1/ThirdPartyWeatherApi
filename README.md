1b76864376cfeaa0ec5a1eb56286b2a63c6c7767
Charlie <charlieboyle252@gmail.com>
2024-04-17 14:33:22 -0500

    new additions of previous search card and css changes

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+
+sources:
+body background image: https://i.pinimg.com/originals/51/ea/69/51ea69d511899d2dae67fc2b3dc65734.jpg
+card background image: https://duckduckgo.com/?q=clear+background&iar=images&iax=images&ia=images&iai=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp6680277.jpg
--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,108 @@
 const APIKey = "c3e27e7ff4cd6c7894a041ab35583de6"
-console.log(APIKey)
 const weatherFinder = document.querySelector(".weatherfinder")
 const displayCard = document.querySelector(".card")
 const currentDay = document.querySelector("#weather-card")
 const fiveDay = document.querySelector(".fiveDay")
+const submitButton = document.querySelector("#submit-button")
+const previousSearches = document.querySelector("#searches")
+
 var savedCities = JSON.parse(localStorage.getItem("saved-cities"))
-if(savedCities == null){
+if (savedCities == null) {
     savedCities = []
     localStorage.setItem("saved-cities", JSON.stringify(savedCities))
 }
-console.log("Saved cities: ", savedCities)
-// String - "" or '' or ``
-// Number  - 5 or 5.5
-// Boolean - true or false
-// Array - [element1, element2]
-// Object - {key1: value1, key2:value2}
+
 function cityInfo(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`
 
     fetch(apiUrl)
-    .then(function(response) {
-        return response.json()
-    }).then( function(response) {
-        console.log(response)
-        currentDay.innerHTML += `
+        .then(function (response) {
+            return response.json()
+        }).then(function (response) {
+            currentDay.innerHTML = ""
+            currentDay.innerHTML += `
         <h3>Temp: ${response.main.temp} ºF
         </h3>
         <p>Humidity: ${response.main.humidity} %
         </p>
+        <p>Wind: ${response.wind.speed} mph
+            </p>
         `
-        console.log(response.main.temp)
-        console.log(response.main.humidity)
-        forecastInfo(response.coord.lat, response.coord.lon)
-    })
-}
-    function forecastInfo(lat, lon){
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
-        fetch(apiUrl)
-        .then(function(response){
-            console.log("response: ", response)
-            return response.json()
-    }).then( function(data) {
-        console.log("Data: ", data)
-        retrieveData(data)
+        
+            forecastInfo(response.coord.lat, response.coord.lon)
         })
-    }
+}
+function forecastInfo(lat, lon) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            retrieveData(data)
+        })
+}
 
-    function retrieveData(data){
-        //use for current day
-        console.log(data.list[0])
-        // use for appending data for current day
+function retrieveData(data) {
 
-
-        for (let i = 0;i <= data.list.length; i += 8){ 
-        let weatherData = data.list[i] 
-        console.log("Weather Data: ", weatherData)
-            fiveDay.innerHTML += `<div class="fiveday-card"> 
-            <h3>Temp: ${weatherData.main.temp} ºF
-            </h3>
+    fiveDay.innerHTML = ""
+    for (let i = 0; i < data.list.length; i += 8) {
+        let weatherData = data.list[i]
+        fiveDay.innerHTML += `<div class="fiveday-card"> 
+            <h4> ${weatherData.dt_txt}
+            </h4>
+            <p>Temp: ${weatherData.main.temp} ºF
+            <p>
             <p>Humidity: ${weatherData.main.humidity} %
             </p>
+            <p>Wind: ${weatherData.wind.speed} mph
+            </p>
             </div>`
-        }
     }
+}
 
-    function saveToStorage(newCity){
-        if(savedCities.includes(newCity)){
-            return
-        }
-        savedCities.push(newCity)
-        localStorage.setItem("saved-cities", JSON.stringify(savedCities))
+function saveToStorage(newCity) {
+    if (savedCities.includes(newCity)) {
+        return
     }
+    savedCities.push(newCity)
+    localStorage.setItem("saved-cities", JSON.stringify(savedCities))
+}
 
+function retrieve5Day(data) {
 
-    function retrieve5Day(data){
+}
+
+function displaySearches() {
+    const cities = JSON.parse(localStorage.getItem("saved-cities"))
+    if (!cities) {
+        return
 
     }
-saveToStorage("Chicago")
-saveToStorage("Dallas")
+    previousSearches.innerHTML = ""
+    for (let i = 0; i < cities.length; i++) {
+        previousSearches.innerHTML += `<div id="searches">
+    <p> ${cities[i]} <p>
+    </div>`
+
+    }
+}
+
+function getCityName() {
+    var inputValue = document.getElementById("city-input")
+    var cityValue = inputValue.value.trim()
+    
+
+    cityInfo(cityValue)
+    saveToStorage(cityValue)
+    displaySearches()
+    inputValue.value = "";
+}
+
 cityInfo("Chicago")
+displaySearches()
 
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    getCityName();
+    displaySearches();
+})
